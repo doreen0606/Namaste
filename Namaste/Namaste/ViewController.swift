4c53aa77136a5d6de73e48516c54f4277d4e972a
Doreen <doreenchen0606@gmail.com>
2025-03-01 10:50:39 +0800

    change: remove storyboard

---
+++ Namaste/Namaste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Namaste
+//
+//  Created by 陳憶婷 on 2025/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Namaste/NamasteTests/NamasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  NamasteTests.swift
+//  NamasteTests
+//
+//  Created by 陳憶婷 on 2025/2/23.
+//
+
+import Testing
+@testable import Namaste
+
+struct NamasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
